fee8002e783fd51977ce875186ff2260ea28af57
antiyro <bouhadananathan@gmail.com>
2021-01-04 08:48:15 +0100

    update

--- ft_isequal.c
+++ ft_isequal.c
@@ -1,41 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_isequal.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbouhada <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/18 17:33:48 by nbouhada          #+#    #+#             */
-/*   Updated: 2020/11/22 14:37:38 by nbouhada         ###   ########.fr       */
+/*   Created: 2020/11/28 11:33:55 by nbouhada          #+#    #+#             */
+/*   Updated: 2020/12/16 16:02:19 by nbouhada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char				*ft_substr(char const *s, unsigned int start, size_t len)
+int		ft_isequal(char *s1, char *s2)
 {
-	unsigned int	i;
-	char			*new;
+	int i;
 
-	if (!s)
-		return (NULL);
-	if (ft_strlen(s) < start)
-	{
-		if (!(new = (char *)malloc(sizeof(char) * 1)))
-			return (NULL);
-		new[0] = '\0';
-		return (new);
-	}
-	if (!(new = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
 	i = 0;
-	while (s[start] && len > 0)
+	while (s1[i])
 	{
-		new[i] = s[start];
+		if (s1[i] != s2[i])
+			return (0);
 		i++;
-		start++;
-		len--;
 	}
-	new[i] = '\0';
-	return (new);
+	if (s2[i] != '\0')
+		return (0);
+	return (1);
 }
--- libft.h
+++ libft.h
@@ -6,12 +6,14 @@
 /*   By: nbouhada <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 10:56:52 by nbouhada          #+#    #+#             */
-/*   Updated: 2020/11/23 15:39:23 by nbouhada         ###   ########.fr       */
+/*   Updated: 2020/12/02 15:54:40 by nbouhada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+// LIBRAIRIES
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -19,11 +21,35 @@
 # include <string.h>
 # include <unistd.h>
 
+// TEXT COLORS AND FONTS
+
+#define RESET   	"\033[0m"
+#define BLACK   	"\033[30m"      	/* Black */
+#define RED     	"\033[31m"      	/* Red */
+#define GREEN   	"\033[32m"      	/* Green */
+#define YELLOW  	"\033[33m"      	/* Yellow */
+#define BLUE    	"\033[34m"      	/* Blue */
+#define MAGENTA 	"\033[35m"      	/* Magenta */
+#define CYAN   		"\033[36m"      	/* Cyan */
+#define WHITE   	"\033[37m"      	/* White */
+#define BOLDBLACK   "\033[1m\033[30m"   /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"	/* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"	/* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"	/* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"	/* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"	/* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"	/* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"	/* Bold White */
+
+// STRUCTURES
+
 typedef	struct		s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+// FONCTIONS PROTOTYPES
 
 void				*ft_memset(void *s, int c, size_t n);
 void				ft_bzero(void *s, size_t n);
@@ -70,5 +96,9 @@ void				ft_lstclear(t_list **lst, void (*del)(void*));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 										void (*del)(void *));
+int					ft_isequal(char *s1, char *s2);
+char				*ft_char_tostr(char c);
+void				ft_clean_stdin(void);
+int					ft_check_file_exist(char *name);
 
 #endif
